13dc514542ccea67bf2545c99e380d630322fdb6
swarupkotikalapudi <swarupkotikalapudi@gmail.com>
2024-06-04 23:06:01 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is changed123 :  " + currentTime);
+		  System.out.println("The current local time is changed321 :  " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
