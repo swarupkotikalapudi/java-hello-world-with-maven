dc095a098d97a79016b8f0493fe9e2eb9628a18e
swarupkotikalapudi <swarupkotikalapudi@gmail.com>
2024-06-04 23:27:37 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is changed to test webhook123 :  " + currentTime);
+		  System.out.println("The current local time is changed to test  :  " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
