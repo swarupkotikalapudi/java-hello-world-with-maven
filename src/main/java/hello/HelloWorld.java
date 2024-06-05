62375f102fab52fb1d4387fc8eed96a48aeb78fa
swarupkotikalapudi <swarupkotikalapudi@gmail.com>
2024-06-05 15:34:59 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is changed to  :  " + currentTime);
-        System.out.println("\n\nTesting webhook changes on commit in jenkins");
+        System.out.println("\n\nTesting webhook changes on commit in jenkins 123");
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
