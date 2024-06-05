36a1873c3718343fc972f9a944a958293a35cba8
swarupkotikalapudi <swarupkotikalapudi@gmail.com>
2024-06-05 17:23:48 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,8 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is changed to  :  " + currentTime);
-        System.out.println("\n\nTesting slack integration working....");
+		  System.out.println("The current local time is changed to  :  " + currentTime);        
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
